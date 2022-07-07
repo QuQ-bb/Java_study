abc2e01f70a607186f4602307525a686a74cd491
QuQ-bb <quqsue@gmail.com>
2022-07-07 09:14:22 +0900

    if문 어려워

--- src/com/iu/control/Control2_ex2.java
+++ src/com/iu/control/Control2_ex2.java
@@ -21,18 +21,26 @@ public class Control2_ex2 {
 		ch = (char)(ch+num);
 		System.out.println("ch :" + ch);
 		
-		num = num + ch;	// a=97 b=98 c=99
+		
+		//if문으로 써보자면 만약 num 값을 입력받았어 > 알파벳도 입력받았어 > 그럼 num +ch
+		
+		//대 65-90
+		//소 97-122
+		//ch = (char)(ch+num);
+		//num = num + ch;	// a=97 b=98 c=99
 		//됐는데..위치변경까진 됐는데...숫자를 문자로 변환해야 함
-		char cc = (char)num;		
+		//char cc = (char)num;		
 		//숫자를..char타입으로 변경.. 이걸 어케if문으로 쓰나요 강사님..
 		
 		//숫자 1이 입력되었을때는 문자 a가 문자b로 출력 될수있게 출력하기
-		System.out.println("입력한 알파벳"+ch);
-		System.out.println("입력한 알파벳을 숫자로 변환한 값 " +num);
-		System.out.println("출력된 숫자를  문자로 변환한 값 " + cc);
+		//System.out.println("입력한 알파벳"+ch);
+		//System.out.println("입력한 알파벳을 숫자로 변환한 값 " +num);
+		//System.out.println("출력된 숫자를  문자로 변환한 값 " + ch);
 		// => num:1 출력:b 
 		// => num:2 출력:c
 		
+		
+		//Study Edition
 		
 	}
 
